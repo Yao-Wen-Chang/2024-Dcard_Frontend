34f6b98c9f9ae4e1b1ae875d1859fed4ac751a82
Yao-Wen Chang <changyaowen19980629@gmail.com>
2024-01-27 16:25:51 +1100

    add: first commit

---
+++ app/_components/PostList.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+// import { fetchPosts } from '../services/githubApi';
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+const PostList: React.FC = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const loadPosts = async () => {
+      const fetchedPosts = await fetchPosts();
+      setPosts(fetchedPosts);
+    };
+
+    loadPosts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Blog Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostList;
